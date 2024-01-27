5f22497d3b842729b1192599b165299d3b515a49
geonheechoi <58959798+geonheechoi@users.noreply.github.com>
2024-01-27 17:54:13 +0900

    ed

--- app/(authenticate)/login.js
+++ app/(authenticate)/login.js
@@ -1,17 +1,190 @@
-import { StyleSheet, Text, View,SafeAreaView,Image } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  KeyboardAvoidingView,
+  TextInput,
+  Pressable,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import { MaterialIcons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
 
-const login = () => {
   return (
     <SafeAreaView
-    styles={{flex:1, backgroundColor:"white", alignItems:"center"}}
+      style={{ flex: 1, backgroundColor: "white", alignItems: "center" }}
     >
-        <View style={{height}}
-      <Text>login</Text>
+      <View style={{ height: 200, backgroundColor: "pink", width: "100%" }}>
+        <View
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: 25,
+          }}
+        >
+          <Image
+            style={{ width: 150, height: 80, resizeMode: "contain" }}
+            source={{
+              uri: "https://cdn-icons-png.flaticon.com/128/6655/6655045.png",
+            }}
+          />
+        </View>
+        <Text
+          style={{
+            marginTop: 20,
+            textAlign: "center",
+            fontSize: 20,
+            fontFamily: "sans-serif",
+          }}
+        >
+          Match Mate
+        </Text>
+      </View>
+      <KeyboardAvoidingView>
+        <View style={{ alignItems: "center" }}>
+          <Text
+            style={{
+              fontSize: 17,
+              fontWeight: "bold",
+              marginTop: 25,
+              color: "#F9629F",
+            }}
+          >
+            Log in to your account
+          </Text>
+        </View>
+        <View
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: 20,
+          }}
+        >
+          <Image
+            style={{ width: 100, height: 100, resizeMode: "cover" }}
+            source={{
+              uri: "https://cdn-icons-png.flaticon.com/128/2509/2509078.png",
+            }}
+          />
+        </View>
+        <View style={{ marginTop: 20 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 5,
+              backgroundColor: "#FFC0CB",
+              paddingVertical: 5,
+              borderRadius: 5,
+              marginTop: 30,
+            }}
+          >
+            <MaterialIcons
+              style={{ marginLeft: 8 }}
+              name="email"
+              size={24}
+              color="white"
+            />
+            <TextInput
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+              placeholder="Enter your email"
+              placeholderTextColor={"white"}
+              style={{
+                color: "white",
+                marginVertical: 10,
+                width: 300,
+                fontSize: password ? 17 : 17,
+              }}
+            />
+          </View>
+          <View style={{}}>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 5,
+                backgroundColor: "#FFC0CB",
+                paddingVertical: 5,
+                borderRadius: 5,
+                marginTop: 30,
+              }}
+            >
+              <AntDesign
+                style={{ marginLeft: 8 }}
+                name="lock1"
+                size={24}
+                color="white"
+              />
+              <TextInput
+                password={password}
+                onChangeText={(text) => setPassword(text)}
+                secureTextEntry={true}
+                placeholder="Enter your password"
+                style={{
+                  color: "white",
+                  marginVertical: 10,
+                  width: 300,
+                  fontSize: password ? 17 : 17,
+                }}
+                placeholderTextColor="white"
+              />
+            </View>
+          </View>
+          <View
+            style={{
+              marginTop: 12,
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Text>Keep me logged in</Text>
+            <Text style={{ color: "#007FFF", fontWeight: "500" }}>
+              Forgot Password
+            </Text>
+          </View>
+          <View style={{ marginTop: 50 }} />
+
+          <Pressable
+            style={{
+              width: 200,
+              backgroundColor: "#FFC0CB",
+              borderRadius: 6,
+              marginLeft: "auto",
+              marginRight: "auto",
+              padding: 15,
+            }}
+          >
+            <Text
+              style={{
+                textAlign: "center",
+                color: "white",
+                fontSize: 16,
+                fontWeight: "bold",
+              }}
+            >
+              Login
+            </Text>
+          </Pressable>
+          <Pressable onPress= {()=>router.replace("/register")}style={{ marginTop: 12 }}>
+            <Text style={{ textAlign: "center", color: "gray", fontSize: 16 }}>
+              Do not have account? Sign up
+            </Text>
+          </Pressable>
+        </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default login
+export default Login;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- app/(authenticate)/register.js
+++ app/(authenticate)/register.js
@@ -1,11 +1,178 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
+import { StyleSheet, Text, View ,SafeAreaView,Image,Pressable,TextInput,KeyboardAvoidingView} from 'react-native'
+import React ,{useState}from 'react'
+import { MaterialIcons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
 const register = () => {
+  const [namem, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
   return (
-    <View>
-      <Text>register</Text>
-    </View>
+    <SafeAreaView
+      style={{ flex: 1, backgroundColor: "white", alignItems: "center" }}
+    >
+      <View style={{ height: 200, backgroundColor: "pink", width: "100%" }}>
+        <View
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: 25,
+          }}
+        >
+          <Image
+            style={{ width: 150, height: 80, resizeMode: "contain" }}
+            source={{
+              uri: "https://cdn-icons-png.flaticon.com/128/6655/6655045.png",
+            }}
+          />
+        </View>
+        <Text
+          style={{
+            marginTop: 20,
+            textAlign: "center",
+            fontSize: 20,
+            fontFamily: "sans-serif",
+          }}
+        >
+          Match Mate
+        </Text>
+      </View>
+      <KeyboardAvoidingView>
+        <View style={{ alignItems: "center" }}>
+          <Text
+            style={{
+              fontSize: 17,
+              fontWeight: "bold",
+              marginTop: 25,
+              color: "#F9629F",
+            }}
+          >
+            register to your account
+          </Text>
+        </View>
+        <View
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: 20,
+          }}
+        >
+          <Image
+            style={{ width: 100, height: 100, resizeMode: "cover" }}
+            source={{
+              uri: "https://cdn-icons-png.flaticon.com/128/2509/2509078.png",
+            }}
+          />
+        </View>
+        <View style={{ marginTop: 20 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 5,
+              backgroundColor: "#FFC0CB",
+              paddingVertical: 5,
+              borderRadius: 5,
+              marginTop: 30,
+            }}
+          >
+            <MaterialIcons
+              style={{ marginLeft: 8 }}
+              name="email"
+              size={24}
+              color="white"
+            />
+            <TextInput
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+              placeholder="Enter your email"
+              placeholderTextColor={"white"}
+              style={{
+                color: "white",
+                marginVertical: 10,
+                width: 300,
+                fontSize: password ? 17 : 17,
+              }}
+            />
+          </View>
+          <View style={{}}>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 5,
+                backgroundColor: "#FFC0CB",
+                paddingVertical: 5,
+                borderRadius: 5,
+                marginTop: 30,
+              }}
+            >
+              <AntDesign
+                style={{ marginLeft: 8 }}
+                name="lock1"
+                size={24}
+                color="white"
+              />
+              <TextInput
+                password={password}
+                onChangeText={(text) => setPassword(text)}
+                secureTextEntry={true}
+                placeholder="Enter your password"
+                style={{
+                  color: "white",
+                  marginVertical: 10,
+                  width: 300,
+                  fontSize: password ? 17 : 17,
+                }}
+                placeholderTextColor="white"
+              />
+            </View>
+          </View>
+          <View
+            style={{
+              marginTop: 12,
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Text>Keep me logged in</Text>
+            <Text style={{ color: "#007FFF", fontWeight: "500" }}>
+              Forgot Password
+            </Text>
+          </View>
+          <View style={{ marginTop: 50 }} />
+
+          <Pressable
+            style={{
+              width: 200,
+              backgroundColor: "#FFC0CB",
+              borderRadius: 6,
+              marginLeft: "auto",
+              marginRight: "auto",
+              padding: 15,
+            }}
+          >
+            <Text
+              style={{
+                textAlign: "center",
+                color: "white",
+                fontSize: 16,
+                fontWeight: "bold",
+              }}
+            >
+              Register
+            </Text>
+          </Pressable>
+          <Pressable onPress= {()=>router.replace("/login")}style={{ marginTop: 12 }}>
+            <Text style={{ textAlign: "center", color: "gray", fontSize: 16 }}>
+              Already have an account? Login
+            </Text>
+          </Pressable>
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   )
 }
 
