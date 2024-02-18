0f7e1b492994d2530ba6a6774341ecd15f9654ee
geonheechoi <58959798+geonheechoi@users.noreply.github.com>
2024-02-18 18:46:54 +0900

    이미지 및 스크롤뷰 추가

--- app/(tabs)/bio/index.js
+++ app/(tabs)/bio/index.js
@@ -1,11 +1,56 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,ScrollView,Image,Pressable } from 'react-native'
 import React from 'react'
 
 const index = () => {
   return (
+    <ScrollView>
     <View>
-      <Text>index</Text>
+  
+<Image
+  style={{ width: "100%", height: 200, resizeMode: "cover" }}
+  source={{
+    uri: "https://static.vecteezy.com/system/resources/thumbnails/018/977/074/original/animated-backgrounds-with-liquid-motion-graphic-background-cool-moving-animation-for-your-background-free-video.jpg",
+  }}
+/>
+
+      <View>
+        <View>
+          <Pressable
+          
+          style={{
+            padding: 10,
+            backgroundColor: "#DDA0DD",
+            width: 300,
+            marginLeft: "auto",
+            marginRight: "auto",
+            justifyContent: "center",
+            alignItems: "center",
+            borderRadius: 10,
+            position: "absolute",
+            top: -60,
+            left: "50%",
+            transform: [{ translateX: -150 }],
+          }}>
+            <Image
+              styles={{ 
+                width: 60, 
+                height: 60, 
+                borderRadius:30,
+                resizeMode: "cover" 
+              }}
+              source ={{
+                uri:'https://cdn-icons-png.flaticon.com/128/1946/1946433.png'
+                
+              }}        
+            />
+
+
+
+          </Pressable>
+        </View>
+      </View>
     </View>
+    </ScrollView>
   )
 }
 
